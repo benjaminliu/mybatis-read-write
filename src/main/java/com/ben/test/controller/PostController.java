e8fd4ed55413b9591515ba165f49958719e27681
liuh <yfdream@163.com>
2018-11-13 11:05:57 +0800

    use undertow , stress test

--- src/main/java/com/ben/test/controller/PostController.java
+++ src/main/java/com/ben/test/controller/PostController.java
@@ -1,13 +1,19 @@
 package com.ben.test.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ben.test.dao.PostMapper;
 import com.ben.test.entity.Post;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * — <br>
@@ -18,6 +24,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/post")
 public class PostController {
+    private static Logger logger = LoggerFactory.getLogger(PostController.class);
+
+    private static Cache<String, String> cache = Caffeine.newBuilder()
+            .expireAfterWrite(10, TimeUnit.HOURS)
+            .build();
 
     @Autowired
     private PostMapper postMapper;
@@ -31,9 +42,26 @@ public class PostController {
         return ResponseEntity.ok().build();
     }
 
+    private static final String selectKey = "select";
+
     @RequestMapping("/select")
     public ResponseEntity<?> query() {
-        List<Post> res = postMapper.selectAll();
+//
+//        String json = "[{\"title\":\"d1\",\"description\":\"d1\"},{\"title\":\"d2\",\"description\":\"d2\"}]";
+//        return ResponseEntity.ok(json);
+
+        logger.info("enter");
+
+        String value = cache.asMap().get(selectKey);
+        List<Post> res = null;
+        if (value == null) {
+            res = postMapper.selectAll();
+            value = JSON.toJSONString(res);
+            cache.asMap().put(selectKey, value);
+        } else {
+            res = JSON.parseArray(value, Post.class);
+        }
+        logger.info("exit");
         return ResponseEntity.ok(res);
     }
 }
