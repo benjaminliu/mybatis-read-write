6bc62800fbfdc70171334cf89a7a2e1f712480d2
liuh <yfdream@163.com>
2018-12-11 09:30:35 +0800

    plugin intercepter

--- src/main/java/com/ben/test/config/MybatisConfig.java
+++ src/main/java/com/ben/test/config/MybatisConfig.java
@@ -1,9 +1,15 @@
 package com.ben.test.config;
 
 import com.ben.test.plugin.DynamicDataSourcePlugin;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -13,7 +19,14 @@ import java.util.Properties;
  * @date: 2018/11/12 <br>
  */
 @Configuration
-public class MybatisConfig {
+public class MybatisConfig implements InitializingBean {
+
+    @Autowired
+    private List<SqlSessionFactory> sqlSessionFactoryList;
+
+    @Autowired
+    private DynamicDataSourcePlugin dynamicDataSourcePlugin;
+
     @Bean
     public DynamicDataSourcePlugin dynamicPluginInterceptor() {
         DynamicDataSourcePlugin plugin = new DynamicDataSourcePlugin();
@@ -21,5 +34,17 @@ public class MybatisConfig {
         properties.setProperty("dialect", "mysql");
         plugin.setProperties(properties);
         return plugin;
+    }
+
+
+    //plugin interceptor
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Iterator var3 = this.sqlSessionFactoryList.iterator();
+
+        while (var3.hasNext()) {
+            SqlSessionFactory sqlSessionFactory = (SqlSessionFactory) var3.next();
+            sqlSessionFactory.getConfiguration().addInterceptor(dynamicDataSourcePlugin);
+        }
     }
 }
